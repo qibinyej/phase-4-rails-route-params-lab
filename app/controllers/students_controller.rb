77cec7482adb020a1c7f287879b2d8f7c35f8646
Qibin <qbye0405@gmail.com>
2022-07-21 00:53:22 -0400

    commit tree 93d6a746ead4336efb90b3fefbf25bc4ef1a3789

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,10 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def student
+    byebug
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
